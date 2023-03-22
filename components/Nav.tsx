6a438e7c0caeb7fa68ce75764287818a96665e36
chapsaldduck <koala031532@gmail.com>
2023-03-23 03:32:30 +0900

    create: magazine page, inquiru page

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,24 +1,24 @@
 import symbol from "/public/crowdysymbol.png";
 import Image from "next/image";
-import styles from "./style/Nav.module.css";
+import Link from "next/link";
 export default function Nav() {
   return (
     <nav className="fixed w-full h-8vh bg-ffffff SCoreDreamFont3 bg-ffffff z-50">
       {/* "flex flex-row text-2xl" */}
       <div className="flex text-2vh">
-        <a href="/" className="flex w-150px relative left-5 top-20px">
+        <Link href="/" className="flex w-150px relative left-5 top-20px">
           <Image src={symbol} alt="" className="w-30px h-30px" />
           <span className="text-crowdyColor">크라우디</span>
-        </a>
-        <a href="/" className="relative top-20px left-10">
+        </Link>
+        <Link href="/" className="relative top-20px left-10">
           홈
-        </a>
-        <a href="/magazine" className="relative top-20px left-15">
+        </Link>
+        <Link href="/magazine" className="relative top-20px left-15">
           매거진
-        </a>
-        <a href="/inquire" className="relative top-20px left-20">
+        </Link>
+        <Link href="/inquiry" className="relative top-20px left-20">
           문의하기
-        </a>
+        </Link>
       </div>
     </nav>
   );
---
+++ src/pages/magazine/index.tsx
@@ -0,0 +1,11 @@
+import Space from "components/Space";
+export default function Magazine() {
+  return (
+    <div>
+      <div className="bg-crowdyColor w-full h-screen">
+        <Space />
+        magaizne page
+      </div>
+    </div>
+  );
+}
